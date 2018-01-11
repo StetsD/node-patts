d89b53e0d62ff10daba2bcfc97510c76177a1d76
StetsD <chortstd@gmail.com>
2018-01-11 12:41:40 +0300

    async nexT

---
+++ spider/01-cb/02-cb.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const cache = {};
+
+function consistentReadAsync(filename, cb){
+    if(cache[filename]){
+        process.nextTick(() => cb(cache[filename]));
+    }else{
+        fs.readFile(filename, 'utf8', (err, data) => {
+            cache[filename] = data;
+            cb(data);
+        });
+    }
+}
+
+function createFileReader(filename){
+    let listeners = [];
+
+    consistentReadAsync(filename, val => {
+        listeners.forEach(listener => listener(val));
+    });
+
+    return {
+        onDataReady: listener => {
+            listeners.push(listener);
+        }
+    }
+}
+
+let reader1 = createFileReader('data.txt');
+reader1.onDataReady(data => {
+    console.log('Reader1 reads ' + data);
+
+    let reader2 = createFileReader('data.txt');
+    reader2.onDataReady(data => {
+        console.log('Reader2 reads ' + data);
+    });
+});
