485f151da33df83416a7e801225b0f2358e1279e
StetsD <chortstd@gmail.com>
2018-01-24 13:09:03 +0300

    two mode of strs

--- 05-strs/02-read/index.js
+++ 05-strs/02-read/index.js
@@ -1,3 +1,4 @@
+//discrete mode
 process.stdin
     .on('readable', () => {
         let chunk;
@@ -9,3 +10,14 @@ process.stdin
         }
     })
     .on('end', () => process.stdout.write('End of stream'));
+
+
+//continious mode
+// process.stdin
+//     .on('data', chunk => {
+//         console.log('New data available');
+//         console.log(
+//             `Chunk read: (${chunk.length}) "${chunk.toString()}"`
+//         );
+//     })
+//     .on('end', () => process.stdout.write('End of stream'));
