a45013c29d561c4f0608c0b85c46a749c1160ad2
StetsD <chortstd@gmail.com>
2018-02-02 12:57:29 +0300

    d

---
+++ 07-binding-modules/01-hard-binding/lib/authController.js
@@ -0,0 +1,15 @@
+const authService = require('./authService');
+
+exports.login = (req, res, next) => {
+    authService.login(req.body.username, req.body.password,
+        (err, result) => {
+            //...
+        }
+    );
+};
+
+exports.checkToken = (req, res, next) => {
+    authService.checkToken(req.query.token, (err, result) => {
+
+    });
+};
