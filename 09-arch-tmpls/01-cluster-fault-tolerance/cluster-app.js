9bdcfeb1496ee45566536fc72083158b6b8ed8a0
StetsD <chortstd@gmail.com>
2018-02-08 08:28:51 +0300

    fault tolerance

---
+++ 09-arch-tmpls/01-cluster-fault-tolerance/cluster-app.js
@@ -0,0 +1,25 @@
+const cluster = require('cluster');
+const os = require('os');
+
+if(cluster.isMaster){
+    const cpus = os.cpus().length;
+    console.log(`Clustering to ${cpus} CPUS`);
+    for(let i = 0; i < cpus; i++){
+        cluster.fork();
+    }
+    Object.keys(cluster.workers).forEach(id => {
+        cluster.workers[id].send('Hello Cluster ' + id);
+    });
+
+	cluster.on('exit', (worker, code) => {
+		if(code != 0 && !worker.suicide){
+			console.log('Worker crashed. Starting a new worker');
+			cluster.fork();
+		}
+	});
+}else{
+    require('./app');
+    process.on('message', msg => {
+        console.log(process.pid, msg);
+    });
+}
